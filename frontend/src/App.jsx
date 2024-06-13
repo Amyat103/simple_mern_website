752371d7248c108823d82a3a99ce2d73df72885e
Amyat103 <zhoudavid7312@gmail.com>
2024-06-13 19:21:21 -0400

    removed file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className='pages'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
